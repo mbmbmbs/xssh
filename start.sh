c6db1f9fec84d73f7e577950f16d8530b8636b74
mbmbmbs <142782141+mbmbmbs@users.noreply.github.com>
2023-09-09 21:10:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,14 +2,5 @@
 source /app/config.sh
 
 service ssh start
-service nginx start
 
-wget https://github.com/mbmbmbs/xssh/releases/download/frpc/frpc
-wget https://github.com/mbmbmbs/xssh/releases/download/frpc/frpc.ini
-wget https://github.com/mbmbmbs/xssh/releases/download/frpc/frpc_full.ini
 frpc -c frpc.ini
-
-#echo "set ngrok token: $NGROK_TOKEN"
-#ngrok authtoken $NGROK_TOKEN
-#echo "start ngrok service"
-#ngrok tcp 22 --log=stdout > ngrok.log
