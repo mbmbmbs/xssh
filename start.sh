3505c56ad16ecbee14821357a6f87f5dde71a24d
mbmbmbs <142782141+mbmbmbs@users.noreply.github.com>
2023-09-09 20:40:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,9 @@ source /app/config.sh
 service ssh start
 service nginx start
 
-echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
-echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+frpc -c frpc.ini
+
+#echo "set ngrok token: $NGROK_TOKEN"
+#ngrok authtoken $NGROK_TOKEN
+#echo "start ngrok service"
+#ngrok tcp 22 --log=stdout > ngrok.log
